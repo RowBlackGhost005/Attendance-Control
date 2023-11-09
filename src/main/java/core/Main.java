b0f88a79536a6713649ab889adcd921905d5d597
GARRIDO\giova <giovanni.garrido228724@potros.itson.edu.mx>
2023-11-09 15:02:11 -0700

    pop up base

--- src/main/java/core/Main.java
+++ src/main/java/core/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         
-        popup.popUpAviso.mostrarAviso("la cagaste", "aviso");
-        
+        popup.popUpAviso.mostrarAviso("Falla X", "Adevertencia X","/img/advertencia.png");
+
     }
 }
--- src/main/java/popup/popUpAviso.java
+++ src/main/java/popup/popUpAviso.java
@@ -1,7 +1,9 @@
 package popup;
 
 import java.awt.Dimension;
+import java.awt.Image;
 import java.awt.Toolkit;
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
@@ -15,19 +17,25 @@ import javax.swing.JTextPane;
  */
 public class popUpAviso {
 
-    public static void mostrarAviso(String mensaje, String titulo) {
+    public static void mostrarAviso(String mensaje, String titulo, String rutaImagen) {
 
         JFrame frame = new JFrame(titulo);
         JPanel panel = new JPanel();
-        
+
         // Agregar etiqueta
-        
         JLabel etiqueta = new JLabel(mensaje);
         panel.add(etiqueta);
-        
 
+        // Cargar la imagen y ajustar su tamaño
+        ImageIcon imagenIcon = new ImageIcon(rutaImagen);
+        Image imagen = imagenIcon.getImage();
+        Image imagenRedimensionada = imagen.getScaledInstance(150, 150, Image.SCALE_SMOOTH);
+        ImageIcon imagenRedimensionadaIcon = new ImageIcon(imagenRedimensionada);
 
-        
+        // Crear una etiqueta para mostrar la imagen
+        JLabel imagenLabel = new JLabel(imagenRedimensionadaIcon);
+        panel.add(imagenLabel);
+
         frame.add(panel);
         // Centrar en la pantalla
         frame.setSize(400, 200); // Establece el tamaño deseado
