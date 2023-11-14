28d6f2a30fcc5c5b26070bd4565d6e1ad830fc6c
GARRIDO\giova <giovanni.garrido228724@potros.itson.edu.mx>
2023-11-13 19:04:17 -0700

    agregar imagen

--- src/main/java/core/Main.java
+++ src/main/java/core/Main.java
@@ -1,11 +1,16 @@
 package core;
 
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("Hello world!");
         
-        popup.popUpAviso.mostrarAviso("Falla X", "Adevertencia X","/img/advertencia.png");
+        popup.popUpAviso.mostrarAviso("Falla X", "Adevertencia X","src/main/java/img/advertencia.png");
+
 
     }
 }
