f31ffa95afe11ee5f360b6522597887e27534476
DanielSalas11 <daniel_salas11@outlook.com>
2023-11-23 17:20:29 -0700

    Se eliminó código comentado

--- src/main/java/core/grupo.java
+++ src/main/java/core/grupo.java
@@ -1,36 +1,15 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package core;
 
-/**
- *
- * @author danielsalas
- */
 public class grupo {
     String nombre;
     String nombreDelCurso;
     String[] horario;
-    // Alumno alumno;
-    // Maestro maestro;
     
     public grupo(String nombre, String nombreDelCurso, String[] horario){
         nombre = this.nombre;
         nombreDelCurso = this.nombreDelCurso;
         horario = this.horario;
     }
-    
-    //TODO Clase que contenga alumnos y maestros para poder hacer un nuevo constructor de la siguiente manera:
-    /*
-    public grupo(String nombre, String nombreDelCurso, String[] horario, Alumno[] alumnos, Maestro maestro){
-        nombre = this.nombre;
-        nombreDelCurso = this.nombreDelCurso;
-        horario = this.horario;
-        alumnos = this.alumnos;
-        maestro = this.maestro
-    }
-    */
 
     public String getNombre() {
         return nombre;
