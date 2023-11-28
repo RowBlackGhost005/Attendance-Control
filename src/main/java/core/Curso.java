ae146c464933abe8560b710e3c7117e9724beffd
DanielSalas11 <daniel_salas11@outlook.com>
2023-11-28 15:48:31 -0700

    Se creó la clase curso, con su constructor, getters y setters.

---
+++ src/main/java/core/Curso.java
@@ -0,0 +1,29 @@
+package core;
+
+public class Curso {
+
+    String nombre;
+    grupo grupos[];
+    
+    public Curso(String nombre, grupo grupos[]){
+        this.nombre = nombre;
+        this.grupos = grupos;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public grupo[] getGrupos() {
+        return grupos;
+    }
+
+    public void setGrupos(grupo[] grupos) {
+        this.grupos = grupos;
+    }
+    
+}
