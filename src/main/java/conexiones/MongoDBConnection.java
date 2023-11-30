794bd52edf5fcba39785a5badff652672ff7e0ed
Felipe Torres <felipedejesus1207@gmail.com>
2023-11-30 16:57:54 -0700

    Update de la clase MongoDBconnection

--- src/main/java/conexiones/MongoDBConnection.java
+++ src/main/java/conexiones/MongoDBConnection.java
@@ -1,30 +1,38 @@
 package conexiones;
 
+import com.mongodb.ConnectionString;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
-
-import org.bson.Document;
-
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
 
 public class MongoDBConnection {
-    private static final String DATABASE_HOST = "localhost"; 
-    private static final int DATABASE_PORT = 27017; 
+    
+    private static final String ATLAS_CONNECTION_STRING = "mongodb+srv://attendanceControl:iUMOsyRYbN2RtWQR@isw-projects.e8av5da.mongodb.net/";
+    private static MongoClient instance;
 
-    public static MongoClient connect() {
-        return MongoClients.create("mongodb://" + DATABASE_HOST + ":" + DATABASE_PORT);
+    private MongoDBConnection() {
+    }
+
+    public static MongoClient getInstance() {
+        if (instance == null) {
+            synchronized (MongoDBConnection.class) {
+                if (instance == null) {
+                    instance = MongoClients.create(new ConnectionString(ATLAS_CONNECTION_STRING));
+                }
+            }
+        }
+        return instance;
     }
 
     public static MongoDatabase getDatabase(String databaseName) {
-        MongoClient client = connect();
-        return client.getDatabase(databaseName);
+        return getInstance().getDatabase(databaseName);
     }
 
-      public static void guardarDocumento(String databaseName, String collectionName, Document documento) {
+    public static void guardarDocumento(String databaseName, String collectionName, Document documento) {
         try {
-            MongoClient client = connect();
-            MongoDatabase database = client.getDatabase(databaseName);
+            MongoDatabase database = getDatabase(databaseName);
             MongoCollection<Document> collection = database.getCollection(collectionName);
             collection.insertOne(documento);
             System.out.println("Documento insertado correctamente.");
@@ -35,8 +43,7 @@ public class MongoDBConnection {
 
     public static void eliminarDocumentos(String databaseName, String collectionName, Document filtro) {
         try {
-            MongoClient client = connect();
-            MongoDatabase database = client.getDatabase(databaseName);
+            MongoDatabase database = getDatabase(databaseName);
             MongoCollection<Document> collection = database.getCollection(collectionName);
             collection.deleteMany(filtro);
             System.out.println("Documentos eliminados correctamente.");
@@ -47,8 +54,7 @@ public class MongoDBConnection {
 
     public static void editarDocumentos(String databaseName, String collectionName, Document filtro, Document actualizacion) {
         try {
-            MongoClient client = connect();
-            MongoDatabase database = client.getDatabase(databaseName);
+            MongoDatabase database = getDatabase(databaseName);
             MongoCollection<Document> collection = database.getCollection(collectionName);
             collection.updateMany(filtro, actualizacion);
             System.out.println("Documentos actualizados correctamente.");
@@ -56,5 +62,6 @@ public class MongoDBConnection {
             e.printStackTrace();
         }
     }
-
 }
+
+
